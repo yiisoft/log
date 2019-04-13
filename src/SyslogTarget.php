1cfdd15732e2023942576b744b4da4ac68a39317
Mehdi Achour <machour@gmail.com>
2019-04-13 21:51:35 +0100

    Fix syslog

--- src/SyslogTarget.php
+++ src/SyslogTarget.php
@@ -8,7 +8,6 @@
 namespace yii\log;
 
 use Psr\Log\LogLevel;
-use yii\helpers\Yii;
 
 /**
  * SyslogTarget writes log to syslog.
@@ -27,6 +26,14 @@ class SyslogTarget extends Target
      */
     public $facility = LOG_USER;
     /**
+     * @var int openlog options. This is a bitfield passed as the `$option` parameter to [openlog()](http://php.net/openlog).
+     * Defaults to `null` which means to use the default options `LOG_ODELAY | LOG_PID`.
+     * @see http://php.net/openlog for available options.
+     * @since 2.0.11
+     */
+    public $options = LOG_ODELAY | LOG_PID;
+
+    /**
      * @var array syslog levels
      */
     private $_syslogLevels = [
@@ -40,25 +47,6 @@ class SyslogTarget extends Target
         LogLevel::DEBUG => LOG_DEBUG,
     ];
 
-    /**
-     * @var int openlog options. This is a bitfield passed as the `$option` parameter to [openlog()](http://php.net/openlog).
-     * Defaults to `null` which means to use the default options `LOG_ODELAY | LOG_PID`.
-     * @see http://php.net/openlog for available options.
-     * @since 2.0.11
-     */
-    public $options;
-
-
-    /**
-     * {@inheritdoc}
-     */
-    public function init()
-    {
-        parent::init();
-        if ($this->options === null) {
-            $this->options = LOG_ODELAY | LOG_PID;
-        }
-    }
 
     /**
      * Writes log messages to syslog.
@@ -79,7 +67,7 @@ class SyslogTarget extends Target
     /**
      * {@inheritdoc}
      */
-    public function formatMessage($message): string
+    public function formatMessage(array $message): string
     {
         [$level, $text, $context] = $message;
         $level = Logger::getLevelName($level);

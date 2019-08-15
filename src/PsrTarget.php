1a95d5f3aaa814ebf8974f18a7aabd10f930c601
Alexander Makarov <sam@rmcreative.ru>
2019-08-15 16:39:55 +0300

    Adjust phpdoc

--- src/PsrTarget.php
+++ src/PsrTarget.php
@@ -5,31 +5,7 @@ use Psr\Log\LoggerInterface;
 
 /**
  * PsrTarget is a log target which simply passes messages to another PSR-3 compatible logger,
- * which is specified via [[$logger]].
- *
- * Application configuration example:
- *
- * ```php
- * return [
- *     'logger' => [
- *         'targets' => [
- *             [
- *                 '__class' => Yiisoft\Log\PsrTarget::class,
- *                 'logger' => function () {
- *                     $logger = new \Monolog\Logger('my_logger');
- *                     $logger->pushHandler(new \Monolog\Handler\SlackHandler('slack_token', 'logs', null, true, null, \Monolog\Logger::DEBUG));
- *                     return $logger;
- *                 },
- *             ],
- *         ],
- *         // ...
- *     ],
- *     // ...
- * ];
- * ```
- *
- * > Warning: make sure logger specified via [[$logger]] is not the same as the Yii logger, otherwise
- *   your program may fall into infinite loop.
+ * which is specified via constructor.
  */
 class PsrTarget extends Target
 {

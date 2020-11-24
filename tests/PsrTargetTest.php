c7cb2a394f80174513d66c6527ff2b9f3ba2191d
devanych <mail@devanych.ru>
2020-11-24 14:12:25 +0300

    Add PsrTargetTest

---
+++ tests/PsrTargetTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Log\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Psr\Log\LogLevel;
+use stdClass;
+use Yiisoft\Log\PsrTarget;
+
+use function json_encode;
+
+class PsrTargetTest extends TestCase
+{
+    private PsrTarget $target;
+
+    public function setUp(): void
+    {
+        $this->target = new PsrTarget(new class () implements LoggerInterface {
+            use LoggerTrait;
+
+            public function log($level, $message, array $context = []): void
+            {
+                echo "{$level}: {$message}: " . json_encode($context);
+            }
+        });
+    }
+
+    public function messageProvider(): array
+    {
+        return [
+            LogLevel::EMERGENCY => [LogLevel::EMERGENCY, 'emergency message', []],
+            LogLevel::ALERT => [LogLevel::ALERT, 'alert message', ['foo' => 'bar']],
+            LogLevel::CRITICAL => [LogLevel::CRITICAL, 'critical message', ['foo' => 1]],
+            LogLevel::ERROR => [LogLevel::ERROR, 'error message', ['foo' => 1.1]],
+            LogLevel::WARNING => [LogLevel::WARNING, 'warning message', ['foo' => 1.1]],
+            LogLevel::NOTICE => [LogLevel::NOTICE, 'notice message', ['foo' => true]],
+            LogLevel::INFO => [LogLevel::INFO, 'info message', ['foo' => ['bar' => 'baz']]],
+            LogLevel::DEBUG => [LogLevel::DEBUG, 'debug message', ['foo' => new stdClass()]],
+        ];
+    }
+
+    /**
+     * @dataProvider messageProvider
+     *
+     * @param string $level
+     * @param string $message
+     * @param array $context
+     */
+    public function testPsrLogInterfaceMethods(string $level, string $message, array $context): void
+    {
+        $this->target->setMessages([[$level, $message, $context]]);
+        $this->assertInstanceOf(LoggerInterface::class, $this->target->getLogger());
+
+        $this->target->export();
+        $this->expectOutputString("{$level}: {$message}: " . json_encode($context));
+    }
+}

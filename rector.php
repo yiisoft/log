6eb2c58360844431ea1e6d75ec888650e49a31c4
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-07-30 17:59:55 +0300

    Cleanup GitHub workflows

--- rector.php
+++ rector.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Yiisoft\CodeStyle\Rector\SetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -12,8 +12,8 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     ->withPhpSets(php80: true)
-    ->withRules([
-        InlineConstructorDefaultToPropertyRector::class,
+    ->withSets([
+        SetList::YII_CORE,
     ])
     ->withSkip([
         ClosureToArrowFunctionRector::class,

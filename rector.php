a370dcd3518972981c9a33075025f77a10f2260a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-07-30 18:01:07 +0300

    fix

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Yiisoft\CodeStyle\Rector\SetList;
 
 return RectorConfig::configure()
@@ -14,7 +13,4 @@ return RectorConfig::configure()
     ->withPhpSets(php80: true)
     ->withSets([
         SetList::YII_CORE,
-    ])
-    ->withSkip([
-        ClosureToArrowFunctionRector::class,
     ]);
